2ed566710c67e45308475c9db02b7f98cff44faf
letspython3x <letspython3x@gmail.com>
2018-07-19 15:55:52 +0530

    removed classes and implemented simple functionality

--- pandas_melt_cli.py
+++ pandas_melt_cli.py
@@ -20,41 +20,27 @@ def parse_cmdline():
     return args
 
 
-class MeltCsv(object):
-    def __init__(self, input_file, output_file=None, id_vars=None,
-                 value_vars=None, var_name=None, value_name=None,
-                 col_level=None):
-        self.input_file = input_file
-        self.output_file = output_file or ''
-        self.id_vars = id_vars.split(',')
-        self.value_vars = value_vars.split(',')
-        self.var_name = var_name or 'variable'
-        self.value_name = value_name or 'value'
-        self.col_level = col_level
+def reshape(df, id_vars=None, value_vars=None, var_name=None, value_name=None, col_level=None):
+    id_vars = id_vars.split(',') if isinstance(id_vars, str) else id_vars
+    value_vars = value_vars.split(',') if isinstance(value_vars, str) else value_vars
+    melted = pd.melt(df, id_vars=id_vars, value_vars=value_vars, var_name=var_name,
+                     value_name=value_name, col_level=col_level)
+    return melted
 
-    @cached_property
-    def df(self):
-        dataframe = pd.read_csv(self.input_file)
-        return dataframe
 
-    def melt(self):
-        melted = pd.melt(self.df, id_vars=self.id_vars,
-                         value_vars=self.value_vars, var_name=self.var_name,
-                         value_name=self.value_name, col_level=self.col_level)
-        return melted
-
-    def save(self, df):
-        if self.output_file:
-            df.to_csv(self.output_file)
-        else:
-            print(df)
+def save(df, output_file):
+    if output_file:
+        with open(output_file, 'w+') as fin:
+            df.to_csv(fin)
+    else:
+        print(df)
 
 
 def main():
     args = parse_cmdline()
-    m = MeltCsv(**args)
-    mdf = m.melt()
-    m.save(mdf)
+    dataframe = pd.read_csv(args.input_file)
+    rdf = reshape(dataframe, **args)
+    save(rdf, args.output_file)
 
 
 if __name__ == "__main__":
